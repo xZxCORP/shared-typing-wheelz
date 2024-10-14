d123fd37d0e3defcf5bde99d40d92200991efe44
Léo Turpin <turpin_l@etna-alternance.net>
2024-10-14 20:55:35 +0200

    feat: adding sign algorithm

--- src/transaction/signature.ts
+++ src/transaction/signature.ts
@@ -5,3 +5,14 @@ export const signatureSchema = z.object({
   signAlgorithm: z.string(),
 });
 export type Signature = z.infer<typeof signatureSchema>;
+
+export const supportedSignAlgorithms = [
+  'RSA-SHA256',
+  'RSA-SHA384',
+  'RSA-SHA512',
+  'ECDSA-SHA256',
+  'ECDSA-SHA384',
+  'ECDSA-SHA512',
+] as const;
+
+export type SignAlgorithm = (typeof supportedSignAlgorithms)[number];
