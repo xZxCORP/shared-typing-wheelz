f932ef1b0fcd3f58faf9cebfae45e1f58831fdda
Léo Turpin <turpin_l@etna-alternance.net>
2024-10-28 11:22:10 +0100

    feat: min value

--- src/transaction/vehicle.ts
+++ src/transaction/vehicle.ts
@@ -6,31 +6,31 @@ const coordinatesSchema = z.object({
 });
 
 const locationSchema = z.object({
-  description: z.string(),
+  description: z.string().min(1),
   coordinates: coordinatesSchema,
 });
 
 const sinisterSchema = z.object({
   date: z.coerce.date(),
-  type: z.string(),
-  severity: z.string(),
-  primaryFactor: z.string(),
-  injuryType: z.string(),
-  collisionType: z.string(),
+  type: z.string().min(1),
+  severity: z.string().min(1),
+  primaryFactor: z.string().min(1),
+  injuryType: z.string().min(1),
+  collisionType: z.string().min(1),
   isWeekend: z.boolean(),
   location: locationSchema,
 });
 
 const risksIssuesSchema = z.object({
-  exterior: z.array(z.string()),
-  mechanical: z.array(z.string()),
-  generic: z.array(z.string()),
+  exterior: z.array(z.string().min(1)),
+  mechanical: z.array(z.string().min(1)),
+  generic: z.array(z.string().min(1)),
 });
 
 export const vehicleSchema = z.object({
-  vin: z.string(),
-  constructorName: z.string(),
-  model: z.string(),
+  vin: z.string().min(17).max(17),
+  constructorName: z.string().min(1),
+  model: z.string().min(1),
   year: z.number().int(),
   risks: risksIssuesSchema,
   sinisters: z.array(sinisterSchema),
