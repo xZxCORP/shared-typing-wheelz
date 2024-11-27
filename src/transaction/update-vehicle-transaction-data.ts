73866f73075239580afd5292a601f4635194a0e6
turpin_l <turpin_l@etna-alternance.net>
2024-11-27 19:33:01 +0100

    feat: change the way partial works

--- src/transaction/update-vehicle-transaction-data.ts
+++ src/transaction/update-vehicle-transaction-data.ts
@@ -2,9 +2,27 @@ import { z } from 'zod';
 
 import { vehicleSchema } from './vehicle.js';
 import { vinSchema } from './vin.js';
-
+const deepPartialItems = vehicleSchema
+  .pick({
+    features: true,
+    infos: true,
+    sinisterInfos: true,
+  })
+  //no alternative to deep partial eheh
+  .deepPartial();
+const partialItems = vehicleSchema
+  .pick({
+    history: true,
+    technicalControls: true,
+  })
+  .partial();
+export const updateVehicleTransactionChangesSchema = z.object({
+  ...deepPartialItems.shape,
+  ...partialItems.shape,
+});
 export const updateVehicleTransactionDataSchema = z.object({
   vin: vinSchema,
-  changes: vehicleSchema.omit({ vin: true }).partial(),
+  changes: updateVehicleTransactionChangesSchema,
 });
 export type UpdateVehicleTransactionData = z.infer<typeof updateVehicleTransactionDataSchema>;
+export type UpdateVehicleTransactionChanges = z.infer<typeof updateVehicleTransactionChangesSchema>;
