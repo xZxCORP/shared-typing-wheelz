a2f428aa287a08ffa2b6206906a993320ca0b7be
Léo Turpin <turpin_l@etna-alternance.net>
2024-12-28 13:09:24 +0100

    fix: nullable lastExecution

--- src/chain/chain-stats.ts
+++ src/chain/chain-stats.ts
@@ -3,10 +3,12 @@ import { z } from 'zod';
 import { dateValueSchema } from '../utils/index.js';
 
 export const chainStatsSchema = z.object({
-  lastExecution: z.object({
-    date: z.string(),
-    newTransactions: z.number(),
-  }),
+  lastExecution: z
+    .object({
+      date: z.string(),
+      newTransactions: z.number(),
+    })
+    .nullable(),
   evolution: z.array(dateValueSchema),
 });
 export type ChainStats = z.infer<typeof chainStatsSchema>;
