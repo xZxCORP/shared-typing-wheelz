05ea544b1dfe3ade231c0d7f79a1836956fc0435
Léo Turpin <turpin_l@etna-alternance.net>
2025-01-21 13:33:22 +0100

    fix: coerce date

--- src/transaction/vin-metadatas.ts
+++ src/transaction/vin-metadatas.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const vinMetadatasSchema = z.object({
-  firstTransactionDate: z.string(),
-  lastTransactionDate: z.string(),
+  firstTransactionDate: z.coerce.date(),
+  lastTransactionDate: z.coerce.date(),
 });
 
 export type VinMetadatas = z.infer<typeof vinMetadatasSchema>;
